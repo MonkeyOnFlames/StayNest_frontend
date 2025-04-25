fbf469356ff342d342d996861d270b3ad2c9fd71
Adam Alkaffagi <adam_1986@live.se>
2025-04-25 12:46:15 +0200

    created text based home page

---
+++ src/home/Home.jsx
@@ -0,0 +1,16 @@
+
+const Home = ()=> {
+
+    return (
+        <div>
+            <h1>StayNest</h1>
+            <h2>Intro</h2>
+            <nav>Search bar</nav>
+            <h2>Filters</h2>
+            <h2>Listings</h2>
+            <footer>Footer</footer>
+        </div>
+    )
+}
+
+export default Home;
