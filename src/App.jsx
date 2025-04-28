363cdfaa3c8225fd306f7d87d77831d55e0067fc
denlillamurmeln <rooba87@gmail.com>
2025-04-28 13:52:15 +0200

    started with login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Home from "./home/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./home/Home";
+import Login from "./login/Login";
 
 function App() {
-  
-
   return (
-    <>
-      <Home />
-    </>
-  )
+    <BrowserRouter>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/login/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import "./auth.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  // konsumerar contexten
+  const { login } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await login(username, password);
+      navigate("/");
+    } catch (err) {
+      console.log("error: " + err);
+    }
+  };
+
+  return (
+    
+    <div> 
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="button">
+          <button>Login</button>
+          {/* <Button text="Login" type="submit" variant="auth">
+              Login
+            </Button> */}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
