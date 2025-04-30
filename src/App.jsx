43a7ad695c8dca5a14741b90d1ad8189a3e54c51
Patrik Turemark <p.turemark@gmail.com>
2025-04-30 11:10:30 +0200

    done with button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./home/Home";
 import Login from "./login/Login";
 import Register from "./login/Register";
 import { AuthProvider } from "./contexts/AuthContext";
+import Button from "./button/button";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <>
+          <Button text={"Register"} width={10} height={2.5} fontSize={1} />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
