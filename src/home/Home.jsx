8e602c771cea64ca0c262a31f097b93a362c638b
denlillamurmeln <rooba87@gmail.com>
2025-05-07 22:38:39 +0200

    made some updates. forgott to make the searchbar dynamic

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,6 +3,38 @@ import InfoSquare from "../infoSquare/InfoSquare";
 import image from "../image/StayNest.jpg";
 import ListingSquare from "../listingSquare/ListingSquare";
 import Searchbar from "../searchbar/Searchbar";
+import Button from "../button/button";
+
+const searchInput = (
+  <input
+    type="text"
+    placeholder="Search for a listing..."
+    className="search-form"
+  />
+);
+
+const filters = [
+  {
+    icon: <div className="search-icon">i</div>,
+    label: "Location",
+    value: "Sweden ▿",
+  },
+  {
+    icon: <div className="search-icon">i</div>,
+    label: "Listing Type",
+    value: "Residence ▿",
+  },
+  {
+    icon: <div className="search-icon">i</div>,
+    label: "Check In",
+    value: "22 May 2025 ▿",
+  },
+  {
+    icon: <div className="search-icon">i</div>,
+    label: "Check Out",
+    value: "30 May 2025 ▿",
+  },
+];
 
 const Home = () => {
   return (
@@ -13,25 +45,23 @@ const Home = () => {
             <h2 style={{ marginBottom: "0.5rem", fontWeight: 400 }}>
               Stay Nest
             </h2>
-            <h2 style={{ margin: 0, fontSize: "2.5rem", fontWeight: 700 }}>
+            <h2 style={{ margin: 0, fontSize: "2.5rem" }}>
               A eco-friendly and <br />
               welcoming stay <br />
-              for travelers
+              for travelers <br />
             </h2>
-            <p
-              style={{
-                marginTop: "1rem",
-                fontSize: "1rem",
-              }}
-            >
-              Every place feels like home
-            </p>
+            <p>Every place feels like home</p>
           </div>
         }
         image={<img src={image} alt="StayNest Logo" />}
       />
 
-<Searchbar/>
+      <Searchbar
+        input={searchInput}
+        filters={filters}
+        button={<Button text="Search" type="submit" width="10" />}
+      />
+
       <nav>Search bar</nav>
       <h2>Filters</h2>
       <ListingSquare name="hest" imageLink={image} environments={["BIKE"]} />
--- src/searchbar/Searchbar.jsx
+++ src/searchbar/Searchbar.jsx
@@ -1,56 +1,21 @@
 import "./searchbar.css";
-import Button from "../button/button";
+//import Button from "../button/button";
 
-const Searchbar = () => {
+const Searchbar = ({ input, filters, button }) => {
   return (
     <div className="search-container">
-      <input
-        type="text"
-        placeholder="Search for a listing..."
-        className="search-form"
-        // value={username}
-        // onChange={(e) => setUsername(e.target.value)}
-      />
+      {<div className="search-input">{input}</div>}
 
-      <div className="search-filter">
-        <div className="search-icon">i</div>
-        <div className="search-filter-text">
-          <div>Location</div>
-          <div className="filter-value">Sweden ▿</div>
+      {filters.map((filter) => (
+        <div className="search-filter" key={1}>
+          {filter.icon}
+          <div className="search-filter-text">
+            <div>{filter.label}</div>
+            <div className="filter-value">{filter.value}</div>
+          </div>
         </div>
-      </div>
-
-      <div className="search-filter">
-        <div className="search-icon">i</div>
-        <div className="search-filter-text">
-          <div>Listing Type</div>
-          <div className="filter-value">Residence ▿</div>
-        </div>
-      </div>
-
-      <div className="search-filter">
-        <div className="search-icon">i</div>
-        <div className="search-filter-text">
-          <div>Check in</div>
-          <div className="filter-value">22 may 2025 ▿</div>
-        </div>
-      </div>
-
-      <div className="search-filter">
-        <div className="search-icon">i</div>
-        <div className="search-filter-text">
-          <div>Check out</div>
-          <div className="filter-value">30 may 2025 ▿</div>
-        </div>
-      </div>
-
-      <Button
-        className="search-button"
-        text="Search"
-        type="submit"
-        //variant="auth"
-        width="10"
-      />
+      ))}
+      <div className="search-button">{button}</div>
     </div>
   );
 };
