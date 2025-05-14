5e81dc81b7ec43bf8e838497ec89c475fb6af054
Patrik Turemark <p.turemark@gmail.com>
2025-05-14 11:48:35 +0200

    made all gets and delete

--- src/api/listingService.js
+++ src/api/listingService.js
@@ -1,6 +1,5 @@
 import api from "./axios";
 
-// get all products
 export const getAllListings = async () => {
   try {
     const response = await api.get("/listings");
@@ -10,3 +9,53 @@ export const getAllListings = async () => {
     throw error;
   }
 };
+
+export const getListingById = async (id) => {
+    try {
+      const response = await api.get(`/listings/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching listing:", error);
+      throw error;
+    }
+  };
+
+  export const getListingsByEnvironment = async () => {
+    try {
+      const response = await api.get("/listings/environments");
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching listing:", error);
+      throw error;
+    }
+  };
+
+  export const getListingBySpecificEnvironment = async (environment) => {
+    try {
+      const response = await api.get(`/listings/${environment}/specific`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching listing:", error);
+      throw error;
+    }
+  };
+
+  export const deleteListingById = async (id) => {
+    try {
+      const response = await api.delete(`/listings/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching listing:", error);
+      throw error;
+    }
+  };
+
+  export const getListingByPriceRange = async (minPrice, maxPrice) => {
+    try {
+      const response = await api.get(`/listings/price?minPrice=${minPrice}&maxPrice=${maxPrice}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching listing:", error);
+      throw error;
+    }
+  };
