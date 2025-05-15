3b0a52a8d9912dee1ed85f513e20968aeb9766ac
denlillamurmeln <rooba87@gmail.com>
2025-05-15 11:04:48 +0200

    fixed upadteUser

--- src/api/userService.js
+++ src/api/userService.js
@@ -1,67 +1,82 @@
-import api from "./axios"
+import api from "./axios";
 
 // get all users
 export const getAllUsers = async () => {
-    try {
-      const response = await api.get("/api/users");
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await api.get("/api/users");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
 
-  // get all users by id
+// get all users by id
 export const getUserById = async (id) => {
-    try {
-      const response = await api.get(`/api/users/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await api.get(`/api/users/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
 
-    // get listing by user id
+// get listing by user id
 export const getUserListings = async (id) => {
-    try {
-      const response = await api.get(`/api/users/${id}/listings`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await api.get(`/api/users/${id}/listings`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
 
-      // get booking by user id
+// get booking by user id
 export const getUserBookings = async (id) => {
-    try {
-      const response = await api.get(`/api/users/${id}/bookings`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await api.get(`/api/users/${id}/bookings`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
 
-    // update a user
-export const updateUser = async (id) => {
-    try {
-      const response = await api.patch(`/api/users/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
-    }
-  };
+// update a user
+export const updateUser = async (
+  id,
+  email,
+  firstName,
+  lastName,
+  phone,
+  adress,
+  age
+) => {
+  try {
+    const response = await api.patch(`/api/users/${id}`, {
+      email,
+      firstName,
+      lastName,
+      phone,
+      adress,
+      age,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
 
-      // delete a user
+// delete a user
 export const deleteUser = async (id) => {
-    try {
-      const response = await api.delete(`/api/users/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await api.delete(`/api/users/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
