b9a2dc25a3ea24ced7feb1973ab5322639c17c6c
Patrik Turemark <p.turemark@gmail.com>
2025-05-19 13:51:32 +0200

    removde SearchBar copy

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,28 @@ import { AuthProvider } from "./contexts/AuthContext";
 import Header from "./header/header";
 import Footer from "./footer/Footer";
 import CreateListingPage from "./createListingPage/createListingPage";
-import SpecificListingPage from "./specificListingPage/SpecificListingPage"
-
-
+import SpecificListingPage from "./specificListingPage/SpecificListingPage";
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <AuthProvider>
         <>
-        <div className="layout">
-        
+          <div className="layout">
+            <Header />
 
-          <Header /> 
-          
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/createListing" element={<CreateListingPage />} />
-            {/* found info on this site: https://reactrouter.com/start/declarative/routing */}
-            <Route path="/listings"><Route path=":id" element={<SpecificListingPage />}/></Route>
-          </Routes>
-          
-          
-          <Footer  />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/createListing" element={<CreateListingPage />} />
+              {/* found info on this site: https://reactrouter.com/start/declarative/routing */}
+              <Route path="/listings">
+                <Route path=":id" element={<SpecificListingPage />} />
+              </Route>
+            </Routes>
+
+            <Footer />
           </div>
         </>
       </AuthProvider>
