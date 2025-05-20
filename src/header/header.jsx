a41fb0ed62baf3332661b5b3742feab03bcf5d1f
Adam Alkaffagi <adam_1986@live.se>
2025-05-20 13:53:47 +0200

    fixing button changes

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -2,32 +2,43 @@ import Button from "../button/button";
 import "./header.css";
 import image from "../image/StayNest.jpg";
 import { Link } from "react-router";
-
+import Login from "../login/Login";
+import { useAuth } from "../hooks/useAuth";
 
 const Header = () => {
+  const { currentUser, logout } = useAuth();
+  const isAdmin =
+    currentUser && currentUser.roles && currentUser.roles.includes("ADMIN");
   return (
     <header className="header">
-      <nav>
-        <div className="logo-container">
-          <Link className="link" to="/">
-            <img src={image} id="headerimg" alt="StayNest logo" />
-          </Link>
-        </div>
-        <div className="linkButton">
-        
-        <Link className="linkLogin" to="/login">
-          <Button text="Login" />
-        </Link>
-        
-        <Link className="linkRegister" to="/register">
-          <Button text="Register" />
-        </Link>
+      <div>
+        <nav>
+          <div className="logo-container">
+            <Link className="link" to="/">
+              <img src={image} id="headerimg" alt="StayNest logo" />
+            </Link>
+          </div>
 
-        <Link className="linkCreateListing" to="/createListing">
-          <Button text="Create Listing" width="9" />
-        </Link>
-        </div>
-      </nav>
+          {currentUser ? (
+            <>
+              <Link to="/">
+                <button className="logout" onClick={handleLogout}>
+                  Logout
+                </button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link className="link" to="/login">
+                Login
+              </Link>
+              <Link className="link" to="/register">
+                Register
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
     </header>
   );
 };
